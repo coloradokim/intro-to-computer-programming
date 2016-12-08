96234ff63a9e579a7413e2557589c990646c9a6a
coloradokim <schlesinger.kim@gmail.com>
2016-12-08 10:44:54 -0700

    added final exercise

--- age.js
+++ age.js
@@ -1,7 +1,17 @@
 function whatCanIDo(age) {
-  if (age === 2) {
+  if (age <= 2) {
     return "You don't need diapers"
   }
 }
+
+// If you are older than 3 "you don't need diapers"
+// If you are older than 15 "you can drive"
+// If you are older than 16 "you can see an R rated movie" If you are older than 17 "you can vote"
+// If you are older than 20 "you can gamble"
+// If you are older than 24 "you can rent a car"
+// If you are older than 49 "you can retire"
+
+// If the age is less than 2, make the code tell you: "Sorry, you are not old enough for anything yet"
+
 
 console.log(whatCanIDo(2));
